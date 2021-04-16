8835cd10456c735d0eff42caa48e8f8d53bfdea7
Nikita Gorokhov <alltroist@gmail.com>
2021-04-17 02:37:48 +0300

    upd src

--- src/validate_model.py
+++ src/validate_model.py
@@ -1,13 +1,21 @@
+import warnings
+
+import pandas as pd
+
 from models.model import ModelSick
 from validator.validator import Validator
+from features.features import generate_features
 
+warnings.filterwarnings('ignore')
 
 def main():
-    validator = Validator(ModelSick(), **{"X": None, "y": None})
-    validator.train_test_split()
-    validator.fit()
-    validator.predict()
-
+    sot = pd.read_csv('data/sotrudniki.csv', sep = ';')
+    rod = pd.read_csv('data/rodstvenniki.csv', sep = ';')
+    ogrv = pd.read_csv('data/OGRV.csv', sep = ';')
+    data = generate_features(sot, rod, ogrv) 
+    validator = Validator(ModelSick(), agrs, kwargs)
+    validator.run(data)
+    print(validator.result)
 
 if __name__ == '__main__':
     main()
