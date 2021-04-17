03c8cb2d852e08509ccd9fbdbf07f5befa85ecba
Nikita Gorokhov <alltroist@gmail.com>
2021-04-17 17:17:02 +0300

    upd validator

--- src/validate_model.py
+++ src/validate_model.py
@@ -1,10 +1,11 @@
+from pprint import pprint
 import warnings
 
 import pandas as pd
 
 from models.model import ModelSick
 from validator.validator import Validator
-from features.features import generate_features
+from features import generate_features
 
 warnings.filterwarnings('ignore')
 
@@ -13,10 +14,11 @@ def main():
     sot = pd.read_csv('data/sotrudniki.csv', sep = ';')
     rod = pd.read_csv('data/rodstvenniki.csv', sep = ';')
     ogrv = pd.read_csv('data/OGRV.csv', sep = ';')
-    data = generate_features(sot, rod, ogrv) 
-    validator = Validator(ModelSick(), agrs, kwargs)
-    validator.run(*data)
-    print(validator.result)
+    X, y = generate_features(sot, rod, ogrv)
+    validator = Validator(ModelSick)
+    validator.run(X, y)
+    pprint(validator.f1_scores)
+    pprint(validator.f1_mean_scores)
 
 if __name__ == '__main__':
     main()
--- src/validator/validator.py
+++ src/validator/validator.py
@@ -1,27 +1,75 @@
+from pprint import pprint
+
+import pandas as pd 
+import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 
 
 class Validator:
-    def __init__(self, args, kwargs):
-        self.model = model(*args, **kwargs)
+    def __init__(self, model, *args, **kwargs):
+        self.model = model(args, kwargs)
+        self.points = [
+            pd.Timestamp(2018, 8, 1),
+            pd.Timestamp(2018, 7, 1),
+            pd.Timestamp(2017, 9, 1),
+            pd.Timestamp(2017, 3, 1),
+            pd.Timestamp(2016, 9, 1),
+        ]
+        self.prediction = []
+        self.f1_scores = []
 
-    def __train_test_split(self, data):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(*data)
+
+    @staticmethod
+    def add_one_year(d):
+        return d + pd.to_timedelta(1, unit='y')
+
+    
+    def __drop_na(self):
+        self.y.dropna(inplace=True)
+        self.X = self.X.loc[self.y.index]
+        self.X['date'] = pd.to_datetime(self.X['date'])
+
+    
+    def __train_test_split(self, point):
+        train_indices = self.X[self.X.date < point].index
+        test_indices = self.X[(self.X.date >= point) & (self.X.date < self.add_one_year(point))].index
+        self.X_train = self.X.loc[train_indices]
+        self.X_test = self.X.loc[test_indices]
+        self.y_train = self.y.loc[train_indices]
+        self.y_test = self.y.loc[test_indices]
+        print(f"For point {point} train size: {self.X_train.shape[0]}, test size: {self.X_test.shape[0]}")
 
     def __fit(self):
-        self.model.fit(X_train, y_train)
+        self.model.fit(self.X_train, self.y_train)
 
     def __predict(self):
-        self.y_pred = self.model.predict(self.X_test, y_test)
+        self.y_pred = self.model.predict(self.X_test)
 
     def __get_metrics(self):
-        self.result = "some metrics"
+        self.prediction.append({"y_test": self.y_test, "y_pred": self.y_pred})
 
-    def run(self, data):
-        self.__train_test_split(data)
-        self.__fit()
-        self.__predict()
-        self.__get_metics()
+    def __calc_f1_score(self):
+        for elem in self.prediction:
+            f1 = []
+            for i in range(1, 13):
+                col_name = f"y_{i}"
+                f1.append(f1_score(elem["y_test"][col_name], elem["y_pred"][col_name]))
+            self.f1_scores.append(f1)
+        self.f1_scores = np.array(self.f1_scores)
+        self.f1_mean_scores = self.f1_scores.mean(axis=0)
+
+
+    def run(self, X, y):
+        self.X = X
+        self.y = y
+        self.__drop_na()
+        for point in self.points:
+            self.__train_test_split(point)
+            self.__fit()
+            self.__predict()
+            self.__get_metrics()
+        self.__calc_f1_score()
 
 
         
